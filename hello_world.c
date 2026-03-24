deb3f23616e4735c1571f9961f4ba7184b973da9
Dorchies_20250072S <florian.wyart@outlook.fr>
2026-03-24 08:15:21 +0100

    Add files via upload

---
+++ hello_world.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+// Déclaration de la fonction qui prend deux entiers et retourne leur somme
+int addition(int a, int b) {
+    return a + b;
+}
+int main() {
+    // PRINT HELLO WORLD
+    printf("Hello, World!\n");
+
+    // COMPARAISON DEUX VARIABLES 
+    int a, b;
+    // Demande à l'utilisateur d'entrer deux nombres
+    printf("Entrez deux nombres : ");
+    scanf("%d %d", &a, &b);
+
+    // Comparaison avec if-else
+    if (a > b) {
+        printf("Le plus grand nombre est : %d\n", a);
+    } else if (b > a) {
+        printf("Le plus grand nombre est : %d\n", b);
+    } else {
+        printf("Les deux nombres sont égaux : %d\n", a);
+    }
+/*
+    //PRINT 1-100  for loop
+    for (int i = 1; i <= 100; i++) {
+         printf("%d\n", i);}
+    
+    //PRINT 1-100 while loop
+    int i = 1; // Initialisation
+
+    while (i <= 100) { // Condition : tant que i ≤ 100
+         printf("%d\n", i);
+         i++; // Incrémentation
+     }
+    
+    // PRINT VALUE AND MEMORY ADDRESS
+    int num = 42; // Déclaration et assignation
+
+    // Affichage de la valeur et de l'adresse mémoire
+    printf("Valeur de num : %d\n", num);
+    printf("Adresse memoire de num : %p\n", (void*)&num);
+
+
+    // FONCTION SUM
+    int num1 = 10, num2 = 20; // Déclaration et assignation de deux nombres
+
+    // Appel de la fonction et affichage du résultat
+    int resultat = addition(num1, num2);
+    printf("La somme de %d et %d est : %d\n", num1, num2, resultat);
+
+    return 0;*/
+    
+}
